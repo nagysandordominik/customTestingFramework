cefd1c77a6bff71a92beb87a1ad2d5d99782aaa8
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-03-18 23:20:37 +0100

    add: running the test files

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log('running test...');
+const Runner = require('./runner');
+
+const runner = new Runner();
+
+const run = async () => {
+    await runner.collectFiles(process.cwd());
+    runner.runTests();   
+};
+
+run();
+
