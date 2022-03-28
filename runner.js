ed77b77830146195688749552a750c4d5712d861
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-03-28 19:51:22 +0200

    add: render function linked

--- runner.js
+++ runner.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+const render = require('./render');
+
+const forbiddenDirs = ['node_modules'];
 
 class Runner {
     constructor() {
@@ -10,6 +13,7 @@ class Runner {
         for (let file of this.testFiles) {
         console.log(chalk.white(`Running tests on ${file.shortName}`))
         const beforeEaches = [];
+        global.render = render;
         global.beforeEach = fn => {
             beforeEaches.push(fn);
         };
@@ -44,7 +48,7 @@ class Runner {
 
             if (stats.isFile() && file.includes('.test.js')) {
                 this.testFiles.push({ name: filepath, shortName: file})
-            } else if (stats.isDirectory()) {
+            } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
                 const childFiles = await fs.promises.readdir(filepath);
 
                 files.push(...childFiles.map(f => path.join(file, f)));
