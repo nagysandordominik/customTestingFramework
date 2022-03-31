1231e43dc157b47ac1520a85e5b6a8d56316a0e4
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-03-31 23:17:59 +0200

    add: dom content loading return with promise

--- render.js
+++ render.js
@@ -2,15 +2,18 @@ const path = require('path');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-const render = async (filename) => {
+const render = async filename => {
     const filePath = path.join(process.cwd(), filename);
 
     const dom = await JSDOM.fromFile(filePath, {
         runScripts: 'dangerously',
         resources: 'usable'
     });
-
-    return dom;
+    return new Promise ((resolve, reject) => {
+        document.addEventListener('DOMContentLoaded', () =>{
+            resolve(dom);
+        });
+    });
 };
 
 module.exports = render;
