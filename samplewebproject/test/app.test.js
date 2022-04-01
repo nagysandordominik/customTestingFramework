ff22e754d59e3fcb49601b94990aec8e7ef9d4c9
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-04-01 21:49:25 +0200

    add: testing for invalid email

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -12,11 +12,26 @@ it('shows a success message with a valid email', async () => {
     const dom = await render ('index.html');
 
     const input = dom.window.document.querySelector('input');
-    input.value = 'dsadasd@asdadasd.com';
+    input.value = 'asd@asd.com';
     dom.window.document
         .querySelector('form')
         .dispatchEvent(new dom.window.Event('submit'));
 
     const h1 = dom.window.document.querySelector('h1');
-    console.log('Contents of h1', h1.innerHTML)
+    
+    assert.strictEqual(h1.innerHTML, 'Looks good!')
+});
+
+it('shows a fail message with a invalid email', async () => {
+    const dom = await render ('index.html');
+
+    const input = dom.window.document.querySelector('input');
+    input.value = 'asd.com';
+    dom.window.document
+        .querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'));
+
+    const h1 = dom.window.document.querySelector('h1');
+    
+    assert.strictEqual(h1.innerHTML, 'Invalid email!')
 });
